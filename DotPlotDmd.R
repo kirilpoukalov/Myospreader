e1bbf385bdbdd0c901cd954cdf07602be900481d
kirilpoukalov <148586736+kirilpoukalov@users.noreply.github.com>
2023-10-31 15:24:47 -0400

    Add files via upload

---
+++ DotPlotDmd.R
@@ -0,0 +1,9 @@
+library(ggplot2)
+df<-data.frame(RTPCR_dmd_data)
+df$Treatment<- factor(df$Treatment, levels=c("PBS", "2xSV40/MDXsg", "Myospreader/MDXsg"))
+df$Muscle<- factor(df$Muscle, levels=c("TA", "Gast","Quad","TRI"))
+p<-ggplot(df, aes(x=Muscle, y=PercentRescue, fill=Treatment)) +
+  geom_dotplot(binaxis='y', stackdir='center',
+               position=position_dodge(.75))+
+  ylim(0,40)
+p + scale_fill_manual(values=c("#37b3f9", "#e39010", "#13a558")) + theme_classic()
